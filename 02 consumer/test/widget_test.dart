574c0cb8bcf8e2d64815eaa31c57290736547d0b
Girts Strazdins <girts.strazdins@gmail.com>
2022-10-16 13:25:44 +0200

    Fix package name in tests

--- 02 consumer/test/widget_test.dart
+++ 02 consumer/test/widget_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:counter/main.dart';
+import 'package:counter_consumer/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
